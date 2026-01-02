46519b37bbaf79289cd9f72ebe2147c1c2680ac4
𝐶𝑆𝑌𝑂𝑁 <134310111+CSY0N@users.noreply.github.com>
2026-01-02 10:56:58 +0100

    Create Production_Boost.cpp
    Added Business Boost

---
+++ src/game/features/recovery/Production_Boost.cpp
@@ -0,0 +1,36 @@
+#include "core/commands/LoopedCommand.hpp"
+#include "game/gta/ScriptGlobal.hpp"
+namespace YimMenu::Features
+{
+	class MansionBusinessBoost : public LoopedCommand
+	{
+		using LoopedCommand::LoopedCommand;
+
+		virtual void OnTick() override
+		{
+			*ScriptGlobal(262145).At(37611).As<int*>() = 86400;
+			*ScriptGlobal(262145).At(37612).As<float*>() = 3.0f; // Meth
+			*ScriptGlobal(262145).At(37613).As<float*>() = 3.0f; // Weed
+			*ScriptGlobal(262145).At(37614).As<float*>() = 3.0f; // Cocaine
+			*ScriptGlobal(262145).At(37615).As<float*>() = 3.0f; // Cash
+			*ScriptGlobal(262145).At(37616).As<float*>() = 3.0f; // Docs
+			*ScriptGlobal(262145).At(37617).As<float*>() = 3.0f; // Bunker
+			*ScriptGlobal(262145).At(37618).As<float*>() = 3.0f; // Acid Lab
+		}
+		virtual void OnDisable() override
+		{
+			*ScriptGlobal(262145).At(37611).As<int*>() = 86400;
+			*ScriptGlobal(262145).At(37612).As<float*>() = 1.0f;
+			*ScriptGlobal(262145).At(37613).As<float*>() = 1.0f;
+			*ScriptGlobal(262145).At(37614).As<float*>() = 1.0f;
+			*ScriptGlobal(262145).At(37615).As<float*>() = 1.0f;
+			*ScriptGlobal(262145).At(37616).As<float*>() = 1.0f;
+			*ScriptGlobal(262145).At(37617).As<float*>() = 1.0f;
+			*ScriptGlobal(262145).At(37618).As<float*>() = 1.0f;
+		}
+	};
+	static MansionBusinessBoost _MansionBusinessBoost{
+	    "mansion_business_boost",
+	    "Mansion Business Boost",
+	    "Boosts all business production"};
+}
