ffa993efa797235a78f2727528ec2c0abe0f19fe
Hexlane <deede3771@gmail.com>
2026-01-04 14:22:59 -0500

    Update Cooldown.cpp

--- src/game/features/recovery/Cooldown.cpp
+++ src/game/features/recovery/Cooldown.cpp
@@ -1,1 +1,51 @@
+#include "core/commands/Command.hpp"
+#include "game/gta/Natives.hpp"
+#include "core/frontend/Notifications.hpp"
+#include "game/gta/Stats.hpp"
+
+namespace YimMenu::Features
+{
+	class ResetCasinoCooldown : public Command
+	{
+		using Command::Command;
+		virtual void OnCall() override
+		{
+			if (*Pointers.IsSessionStarted)
+			{
+				Stats::SetInt("MPPLY_CASINO_CHIPS_WON_GD", 0);
+				Stats::SetInt("MPPLY_CASINO_BAN_TIME", 0);
+				STATS::STAT_SAVE(0, 0, 3, 0);
+			}
+			else
+			{
+				Notifications::Show("YimMenuV2", "You Must be Online", NotificationType::Error);
+			}
+		}
+	};
+	static ResetCasinoCooldown _ResetCasinoCooldown{
+	    "reset_casino_cd",
+	    "Reset Casino Cooldowns",
+	    "Resets casino daily chip limit and gambling ban"};
+	//vehicle reset
+	class ResetVehicleSellCooldown : public Command
+	{
+		using Command::Command;
+		virtual void OnCall() override
+		{
+			if (*Pointers.IsSessionStarted)
+			{
+				Stats::SetInt("MPPLY_VEHICLE_SELL_TIME", 0);
+				STATS::STAT_SAVE(0, 0, 3, 0);
+			}
+			else
+			{
+				Notifications::Show("YimMenuV2", "You Must be Online", NotificationType::Error);
+			}
+		}
+	};
+	static ResetVehicleSellCooldown _ResetVehicleSellCooldown{
+	    "reset_vehicle_sell_cd",
+	    "Reset Vehicle Sell Cooldown",
+	    "Resets the vehicle sell cooldown timer"};
+}
 
