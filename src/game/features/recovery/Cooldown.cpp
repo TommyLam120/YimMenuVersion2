4f00a31d25f737a0a83cb1d18c391693bccaf811
Hexlane <deede3771@gmail.com>
2026-01-08 23:23:35 -0500

    Update Cooldown.cpp

--- src/game/features/recovery/Cooldown.cpp
+++ src/game/features/recovery/Cooldown.cpp
@@ -2,6 +2,10 @@
 #include "game/gta/Natives.hpp"
 #include "core/frontend/Notifications.hpp"
 #include "game/gta/Stats.hpp"
+#include "core/backend/FiberPool.hpp"
+#include "game/gta/ScriptGlobal.hpp"
+#include "types/script/Timer.hpp"
+
 
 namespace YimMenu::Features
 {
@@ -47,4 +51,27 @@ namespace YimMenu::Features
 	    "reset_vehicle_sell_cd",
 	    "Reset Vehicle Sell Cooldown",
 	    "Resets the vehicle sell cooldown timer"};
+
+	class ResetDeliveryCooldown : public Command
+	{
+		using Command::Command;
+		virtual void OnCall() override
+		{
+			if (*Pointers.IsSessionStarted)
+			{
+				FiberPool::Push([] {
+					ScriptGlobal(2686095).At(4357).At(260).At(7, 2).As<TIMER*>()->Destroy();
+				});
+				STATS::STAT_SAVE(0, 0, 3, 0);
+			}
+			else
+			{
+				Notifications::Show("YimMenuV2", "You Must be Online", NotificationType::Error);
+			}
+		}
+	};
+	static ResetDeliveryCooldown _ResetDeliveryCooldown{
+	    "Reset_vehicle_Delivery_cd",
+	    "Reset Vehicle Delivery Cooldown",
+	    "Reset Vehicle Delivery Cooldown"};
 }
