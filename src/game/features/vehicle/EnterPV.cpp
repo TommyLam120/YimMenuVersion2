80481941aa040dd1e99e0e49cd924be3c04c9aac
Hexlane <deede3771@gmail.com>
2026-01-22 11:47:09 -0500

    Update EnterPV.cpp

--- src/game/features/vehicle/EnterPV.cpp
+++ src/game/features/vehicle/EnterPV.cpp
@@ -1,1 +1,28 @@
+#include "core/commands/Command.hpp"
+#include "core/backend/FiberPool.hpp"
+#include "game/backend/PersonalVehicles.hpp"
+#include "game/backend/Self.hpp"
+#include "core/frontend/Notifications.hpp"
 
+namespace YimMenu::Features
+{
+	class EnterPV : Command
+	{
+	  using Command::Command;
+
+	  virtual void OnCall() override
+	  {
+	    FiberPool::Push([] {
+		if (auto veh = PersonalVehicles::GetCurrentHandle(); veh.IsValid())
+		{
+			Self::GetPed().SetInVehicle(veh);
+		}
+		else
+		{
+			Notifications::Show("YimMenuV2","No personal vehicle found",NotificationType::Error);
+		}
+	  });
+    }
+};
+	static EnterPV _EnterPV{"enterpvvehicle", "Enter PV Vehicle", "Enter Your PV Vehicle"};
+};
