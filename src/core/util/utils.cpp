208783b190e9a301c499a128d3254dd073059c2a
Hexlane <deede3771@gmail.com>
2026-01-29 16:49:40 -0500

    Update utils.cpp

--- src/core/util/utils.cpp
+++ src/core/util/utils.cpp
@@ -1,1 +1,7 @@
-
+#include "utils.hpp"
+#include "game/gta/Natives.hpp"
+namespace Utils {
+	void ApplyForceToEntity(Entity entity, float x, float y, float z, float offX, float offY, float offZ) {
+		ENTITY::APPLY_FORCE_TO_ENTITY(entity, 1, x, y, z, offX, offY, offZ, 0, 1, 1, 1, 0, 1);
+	}
+}
