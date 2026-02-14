f6108a7ca0a5bd394053c00322263c9025fbc067
Hexlane <deede3771@gmail.com>
2026-02-13 23:04:01 -0500

    Update utils.cpp

--- src/core/util/utils.cpp
+++ src/core/util/utils.cpp
@@ -4,4 +4,16 @@ namespace Utils {
 	void ApplyForceToEntity(Entity entity, float x, float y, float z, float offX, float offY, float offZ) {
 		ENTITY::APPLY_FORCE_TO_ENTITY(entity, 1, x, y, z, offX, offY, offZ, 0, 1, 1, 1, 0, 1);
 	}
+   void SetHeading(float heading) {
+       Ped ped = PLAYER::PLAYER_PED_ID();
+       if (PED::IS_PED_IN_ANY_VEHICLE(ped, false))
+       {
+           Vehicle veh = PED::GET_VEHICLE_PED_IS_IN(ped, false);
+           ENTITY::SET_ENTITY_HEADING(veh, heading);
+       }
+       else 
+       {
+            ENTITY::SET_ENTITY_HEADING(ped, heading);
+       }
+    }
 }
